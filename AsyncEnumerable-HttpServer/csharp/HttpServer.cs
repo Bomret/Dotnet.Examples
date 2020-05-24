8655637f3544490dc38a6c285f1fddf76e8c6dab
Stefan Reichel <dev@bomret.com>
2020-05-24 15:29:03 +0200

    add add style files

--- AsyncEnumerable-HttpServer/csharp/HttpServer.cs
+++ AsyncEnumerable-HttpServer/csharp/HttpServer.cs
@@ -3,18 +3,14 @@ using System.Net;
 using System.Runtime.CompilerServices;
 using System.Threading;
 
-namespace Bomret.Dotnet.Examples.AsyncEnumerableHttpServer.CSharp
-{
-  public static class HttpServer
-  {
-    public struct Options
-    {
+namespace Bomret.Dotnet.Examples.AsyncEnumerableHttpServer.CSharp {
+  public static class HttpServer {
+    public struct Options {
       public string Hostname;
       public ushort Port;
     }
 
-    public static async IAsyncEnumerable<HttpListenerContext> Serve(Options options, [EnumeratorCancellation] CancellationToken cancellationToken)
-    {
+    public static async IAsyncEnumerable<HttpListenerContext> Serve(Options options, [EnumeratorCancellation] CancellationToken cancellationToken) {
       using var listener = new HttpListener();
 
       string url = $"http://{options.Hostname}:{options.Port}/";
@@ -22,8 +18,7 @@ namespace Bomret.Dotnet.Examples.AsyncEnumerableHttpServer.CSharp
       listener.Prefixes.Add(url);
       listener.Start();
 
-      while (!cancellationToken.IsCancellationRequested)
-      {
+      while (!cancellationToken.IsCancellationRequested) {
         var context = await listener.GetContextAsync().ConfigureAwait(false);
 
         yield return context;
--- AsyncEnumerable-HttpServer/csharp/Program.cs
+++ AsyncEnumerable-HttpServer/csharp/Program.cs
@@ -3,30 +3,24 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Bomret.Dotnet.Examples.AsyncEnumerableHttpServer.CSharp
-{
-  static class Program
-  {
+namespace Bomret.Dotnet.Examples.AsyncEnumerableHttpServer.CSharp {
+  static class Program {
     static readonly ReadOnlyMemory<byte> Message = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes("Hello World"));
 
-    static void Main(string[] args)
-    {
+    static void Main(string[] args) {
       using var cts = new CancellationTokenSource();
       Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs args) => cts.Cancel();
 
       Do(cts.Token).GetAwaiter().GetResult();
     }
 
-    static async Task Do(CancellationToken cancellationToken)
-    {
-      var options = new HttpServer.Options
-      {
+    static async Task Do(CancellationToken cancellationToken) {
+      var options = new HttpServer.Options {
         Hostname = "localhost",
         Port = 8080
       };
 
-      await foreach (var ctx in HttpServer.Serve(options, cancellationToken))
-      {
+      await foreach (var ctx in HttpServer.Serve(options, cancellationToken)) {
         using var outputStream = ctx.Response.OutputStream;
 
         await outputStream.WriteAsync(Message, cancellationToken);
